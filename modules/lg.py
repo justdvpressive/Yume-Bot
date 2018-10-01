7124d33555f94abdc8c8a7bcc00fb252f8f7b79e
yumenetwork <yumenetwork@protonmail.com>
2018-10-01 10:06:27 +0200

    Working on commands

--- modules/lg.py
+++ modules/lg.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 import json
+from itertools import cycle
+import asyncio
+
 
 class Lg:
 
@@ -14,18 +17,41 @@ class Lg:
         conf = config
 
 
+
     @commands.command(pass_context = True)
     @commands.guild_only()
     @commands.has_permissions(manage_messages = True)
-    @commands.cooldown(1, 20, commands.BucketType.user)
-    async def lg(self, ctx, arg: str = None):
+    #@commands.cooldown(1, 20, commands.BucketType.user)
+    async def lg(self, ctx, arg: str = None, iarg: int = None):
 
         msg = ctx.message
+        guild = ctx.message.guild
 
 
         if arg == "setup":
+            await ctx.send("SETUP...")
+            global vchannel
+            vchannel = await guild.create_voice_channel("Lg Register")
+            overwrites = {
+                guild.default_role: discord.PermissionOverwrite(connect = False)
+            }
+            global gchannel
+            gchannel = await guild.create_voice_channel("Game", overwrites = overwrites)
+            # TODO: Faire un channel de jeux pour la narration et des salons invisibles pour chaques roles !
+            return
 
-            await ctx.send("SETUP !")
+
+        elif arg == "start":
+            await ctx.send("Starting...")
+            players = vchannel.members
+            for player in players:
+                await player.move_to(gchannel)
+                await ctx.send("{} has joined the game".format(player.name))
+                return
+
+
+        elif arg == None:
+            await ctx.send("Invalid arg")
 
         else:
             return
